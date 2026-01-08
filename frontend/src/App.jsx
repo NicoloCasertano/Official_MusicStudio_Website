a48a00a36f519ca96ac79336627701fcda11e7de
N9C9 <nicolo.casertano@gmail.com>
2026-01-08 23:09:47 +0100

    new comit 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ import Checkout from './pages/Checkout'
 import Testimonials from './components/testimonials/Testimonials';
 
 import video1 from './assets/video1.mp4';
-import video2 from './assets/video2.mp4' ;
+import video2 from './assets/video2.mp4';
 
 const slides = [
 	{name: '"Il miglior studio nel sud milano" cit. Gigione',video: video1},
